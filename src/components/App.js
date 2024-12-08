502e37d324bdd333fc5f52c3baa68948a290c220
sujitha <sujipinku07@gmail.com>
2024-12-08 15:49:03 +0530

    router

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ItemList from './ItemList'
+import ItemDetail from './ItemDetail'
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+          <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<ItemList />}></Route>
+                <Route path='/items/:id' element={<ItemDetail />} />
+            </Routes>
+        </BrowserRouter>
     </div>
   )
 }
